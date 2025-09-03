87bd92981264e81f4dc620da3bdb832d2dea0fe7
Alex Kolbin <kolbin.alex@gmail.com>
2025-09-03 14:44:28 -0400

    chore: Delete NATs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,9 +9,9 @@ module "vpc" {
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   private_subnets = [for k, v in slice(data.aws_availability_zones.available.names, 0, 3) : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in slice(data.aws_availability_zones.available.names, 0, 3) : cidrsubnet(var.vpc_cidr, 4, k + 4)]
+  public_subnets  = []
 
-  enable_nat_gateway   = true
+  enable_nat_gateway   = false
   single_nat_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
